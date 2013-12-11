a3dc898f7e8a2e015d3ab4a2464ed666bfb5a8a3
madhusmita <madhusmitap@mindfiresolutions.com>
2013-12-11 17:41:59 +0530

    Taken-off un-necessary import files
    Signed-off-by: madhusmita <madhusmitap@mindfiresolutions.com>

--- SpringCore/src/main/java/com/mkyong/service/CustomerService.java
+++ SpringCore/src/main/java/com/mkyong/service/CustomerService.java
@@ -1,7 +1,5 @@
 package com.mkyong.service;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Random;
