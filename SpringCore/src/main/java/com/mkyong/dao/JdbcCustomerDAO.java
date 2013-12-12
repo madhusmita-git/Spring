adbcc141acc7ff15fbba9caf6a857d0e4f578435
madhusmita <madhusmitap@mindfiresolutions.com>
2013-12-12 17:23:50 +0530

    Cleared unused import java files
    Signed-off-by: madhusmita <madhusmitap@mindfiresolutions.com>

--- SpringCore/src/main/java/com/mkyong/dao/JdbcCustomerDAO.java
+++ SpringCore/src/main/java/com/mkyong/dao/JdbcCustomerDAO.java
@@ -1,9 +1,5 @@
 package com.mkyong.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -30,27 +26,6 @@ public class JdbcCustomerDAO extends JdbcDaoSupport implements ICustomerDAO {
 		getJdbcTemplate().update(sql, new Object[] {
 				customer.getCustId(), customer.getName(), customer.getAge()}
 		);
-		/*Connection conn = null;
- 
-		try {
-			conn = dataSource.getConnection();
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setInt(1, customer.getCustId());
-			ps.setString(2, customer.getName());
-			ps.setInt(3, customer.getAge());
-			ps.executeUpdate();
-			ps.close();
- 
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
- 
-		} finally {
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {}
-			}
-		}*/
 	}
 
 	@Override
@@ -66,35 +41,7 @@ public class JdbcCustomerDAO extends JdbcDaoSupport implements ICustomerDAO {
 			customer.setAge((Long)row.get("AGE"));
 		}
 		
-		return customer;		
-		
-		/*Connection conn = null;
- 
-		try {
-			conn = dataSource.getConnection();
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setInt(1, custId);
-			Customer customer = null;
-			ResultSet rs = ps.executeQuery();
-			if (rs.next()) {
-				customer = new Customer(
-					rs.getInt("CUST_ID"),
-					rs.getString("NAME"), 
-					rs.getInt("Age")
-				);
-			}
-			rs.close();
-			ps.close();
-			return customer;
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		} finally {
-			if (conn != null) {
-				try {
-				conn.close();
-				} catch (SQLException e) {}
-			}
-		}*/
+		return customer;
 	}
 
 }
--- SpringCore/src/main/java/com/mkyong/model/Customer.java
+++ SpringCore/src/main/java/com/mkyong/model/Customer.java
@@ -1,7 +1,5 @@
 package com.mkyong.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
